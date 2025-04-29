4716afa5bcf97eb405f774c4fea8f0b19ff0c194
Akshat Biniwale <akshatbiniwale@gmail.com>
2025-04-30 01:30:10 +0530

    cors and port changes for deployment

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 dotenv.config();
 connectDB();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "https://strings-frontend.vercel.app/" }));
 
 // Returns middleware that only parses json and only looks at requests where the Content-Type header matches the type option.
 
@@ -37,7 +37,7 @@ app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 app.use(invalidPathHandler);
 app.use(errorResponseHandler);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log("Server is up and listening on port " + PORT);
